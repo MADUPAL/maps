945e77e2f23adbb9b323cdc681e257ba2cdf0f75
MADUPAL <kevin488@naver.com>
2024-02-14 18:15:21 +0900

    add logo

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -1,16 +1,24 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-3 md:px-2">
+    <div className="flex flex-col h-full px-3 py-3 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-gray-200 p-4 md:h-40"
+        className="mb-2 flex h-20 items-center justify-center rounded-md bg-[#1E1F22] p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
-          
+        <div className="w-32 md:w-40">
+          <Image
+            src="/Path_of_Exile_Logo.png"
+            className="block"
+            width={200}
+            height={100}
+            alt="poe logo"
+            priority={true}
+          />
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
