f84224298af198916eca800cb259913bf5bdc057
MADUPAL <kevin488@naver.com>
2024-02-22 18:08:54 +0900

    delete not used one

--- app/components/page-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function PageWrapper({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex flex-col space-y-4 min-h-screen p-4">
-      {children}
-    </div>
-  );
-}
